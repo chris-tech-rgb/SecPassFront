285e0439d1fe863930834a8c8a3f8441a7af0970
Chris <turcottezenobia@gmail.com>
2023-03-06 18:39:05 +0900

    empty dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,21 +1,13 @@
 <!--suppress CssUnusedSymbol -->
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-  </div>
+  <div class="dashboard-container" />
 </template>
 
 <!--suppress JSAnnotator -->
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
+  name: 'Dashboard'
 }
 </script>
 
