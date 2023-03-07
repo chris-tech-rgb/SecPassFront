09bc48631bd2e8d9c6f440ab1a5ac5b6f4130b2e
Chris <turcottezenobia@gmail.com>
2023-03-07 18:07:50 +0900

    safety

--- src/store/modules/password.js
+++ src/store/modules/password.js
@@ -10,8 +10,16 @@ const actions = {
     const encrypted_website = CryptoJS.AES.encrypt(website.trim(), pin).toString()
     const encrypted_username = CryptoJS.AES.encrypt(username.trim(), pin).toString()
     const encrypted_password = CryptoJS.AES.encrypt(password.trim(), pin).toString()
+    let safety = '🔴'
+    if (password.length >= 10 && /[^a-zA-Z0-9]/.test(password)) {
+      if (password.length >= 16 && /[a-z]/.test(password) && /[A-Z]/.test(password)) {
+        safety = '🟢'
+      } else {
+        safety = '🟡'
+      }
+    }
     return new Promise((resolve, reject) => {
-      submit({ user: user.trim(), website: encrypted_website, username: encrypted_username, password: encrypted_password }).then(() => {
+      submit({ user: user.trim(), website: encrypted_website, username: encrypted_username, password: encrypted_password, safety: safety }).then(() => {
       }).catch(error => {
         reject(error)
       })
