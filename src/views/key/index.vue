dd4c9194f379d1d1c1e8ce74855b5d15ba2424fb
Chris <turcottezenobia@gmail.com>
2023-03-12 18:46:39 +0900

    copy

--- src/views/key/index.vue
+++ src/views/key/index.vue
@@ -23,12 +23,12 @@
       <el-form-item v-if="key_type_selected.password" label="排除易混淆字符">
         <el-switch v-model="password_form.exclude" />
       </el-form-item>
-      <el-form-item label="结果">
-        <el-input v-model="output" type="textarea" />
+      <el-form-item v-if="key_type_selected.password" label="结果">
+        <el-input v-model="password_form.output" type="textarea" />
       </el-form-item>
-      <el-form-item>
+      <el-form-item v-if="key_type_selected.password">
         <el-button type="primary" @click="create">生成</el-button>
-        <el-button @click="copy">复制</el-button>
+        <el-button @click="copy(password_form.output)">复制</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -48,14 +48,14 @@ export default {
       password_form: {
         length: 16,
         type: ['数字', '小写字母', '大写字母', '特殊符号'],
-        exclude: false
-      },
-      output: ''
+        exclude: false,
+        output: ''
+      }
     }
   },
   methods: {
     onChange() {
-      this.output = ''
+      this.password_form.output = ''
       switch (this.key_type) {
         case 'password':
           this.key_type_selected.password = true
@@ -118,26 +118,27 @@ export default {
           result += characters.charAt(Math.floor(Math.random() * characters.length))
           counter += 1
         }
-        this.output = result
+        this.password_form.output = result
       }
     },
-    copy() {
-      // Create new element
-      const el = document.createElement('textarea')
-      // Set value (string to be copied)
-      el.value = this.output
-      el.value = this.output
-      // Set non-editable to avoid focus and move outside of view
-      el.setAttribute('readonly', '')
-      el.style = { position: 'absolute', left: '-9999px' }
-      document.body.appendChild(el)
-      // Select text inside element
-      el.select()
-      // Copy text to clipboard
-      document.execCommand('copy')
-      // Remove temporary element
-      document.body.removeChild(el)
-      this.$message('已复制')
+    copy(key) {
+      if (key.length > 0) {
+        // Create new element
+        const el = document.createElement('textarea')
+        // Set value (string to be copied)
+        el.value = key
+        // Set non-editable to avoid focus and move outside of view
+        el.setAttribute('readonly', '')
+        el.style = { position: 'absolute', left: '-9999px' }
+        document.body.appendChild(el)
+        // Select text inside element
+        el.select()
+        // Copy text to clipboard
+        document.execCommand('copy')
+        // Remove temporary element
+        document.body.removeChild(el)
+        this.$message('已复制')
+      }
     }
   }
 }
